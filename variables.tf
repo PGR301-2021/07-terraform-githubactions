d2a50c2d5a85dec061fccf50bb5a87451a2ad3cb
EC2 Default User <ec2-user@ip-172-31-4-168.eu-north-1.compute.internal>
2021-10-12 08:53:23 +0000

    Configured variable.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,4 @@
-module "static-site" {
-    source  = "telia-oss/static-site/aws"
-    version = "3.0.0"
-    
-    hosted_zone_name = "thecloudcollege.com"
-    name_prefix      = "laba006"
-    site_name        = "laba006.thecloudcollege.com"
+variable "student_name" {
+  default = "laba006"
+  type    = string
 }
