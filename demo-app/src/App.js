3010b101468e69ae4310c8df103f33734eda3b24
Laurent <laurent_baj@hotmail.com>
2021-10-12 12:08:51 +0200

    Small change

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> DevOpsForLife !
+          Edit <code>src/App.js</code> Laurent4Life! !
         </p>
         <a
           className="App-link"
